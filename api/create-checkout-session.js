1a798e95bc473d419f0326f51372eebc568afa9f
cmyzk-git <hbksep20@gmail.com>
2026-06-10 12:50:51 +0900

    add error logging to checkout api

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -6,7 +6,11 @@ module.exports = async (req, res) => {
   }
 
   try {
+    console.log('STRIPE_KEY exists:', !!process.env.STRIPE_SECRET_KEY);
+    console.log('PRICE_ID:', process.env.STRIPE_PRICE_ID);
+
     const { user_id, email } = req.body;
+    console.log('user_id:', user_id, 'email:', email);
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
@@ -23,6 +27,7 @@ module.exports = async (req, res) => {
 
     res.status(200).json({ url: session.url });
   } catch (err) {
+    console.error('Stripe error:', err.message);
     res.status(500).json({ error: err.message });
   }
 };
